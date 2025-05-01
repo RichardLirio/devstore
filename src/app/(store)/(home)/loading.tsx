86138700af80ff66fb884f5ba5d6326d30054fff
Richard Lirio <richardlirio@hotmail.com>
2025-04-30 22:42:16 -0300

    Criando layout da aplicação

---
+++ src/app/(store)/(home)/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomeLoading() {
+  return <div>Caregando...</div>;
+}
+
+export default HomeLoading;
---
+++ src/app/(store)/search/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SearchLoading() {
+  return <div>Caregando Search...</div>;
+}
+
+export default SearchLoading;
